fe63613226be9f71d458467422ff6812bfc027fe
skylakefreak <utkarshbarde2@gmail.com>
2024-10-22 22:57:20 +0530

    s

--- components/Iconrenderer.jsx
+++ components/Iconrenderer.jsx
@@ -5,7 +5,7 @@ function Iconrenderer({link,text,click}) {
   return (
     <div onDoubleClick={click} className='w-[80px] flex flex-col items-center justify-center'>
     <Image className='h-[60px] w-[60px] hover:cursor-pointer hover:opacity-70 transition-all duration-80' src={link}></Image>
-    <p className='text-white text-center text-xs'>{text}</p>
+    <p className='text-white text-center text-xl'>{text}</p>
   </div>
   )
 }
