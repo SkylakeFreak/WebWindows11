2d7e25e20892ee47031f3d35697563e9a710034e
skylakefreak <utkarshbarde2@gmail.com>
2024-10-23 23:00:38 +0530

    s

--- components/Iconrenderer.jsx
+++ components/Iconrenderer.jsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 
 function Iconrenderer({link,text,click}) {
   return (
-    <div onDoubleClick={click} className='w-[80px] flex flex-col items-center justify-center'>
+    <div onDoubleClick={click} className='w-[80px] text-white transition-all duration-300 hover:text-black flex flex-col items-center justify-center'>
     <Image className='h-[60px] w-[60px] hover:cursor-pointer hover:opacity-70 transition-all duration-80' src={link}></Image>
-    <p className='text-white text-center text-xl'>{text}</p>
+    <p className='text-center text-xs'>{text}</p>
   </div>
   )
 }
+
+
+
 
 export default Iconrenderer
