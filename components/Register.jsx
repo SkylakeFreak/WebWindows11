038dba34ae68b53ebb0e1dd0d788b0ee20675d7b
skylakefreak <utkarshbarde2@gmail.com>
2024-12-10 23:34:32 +0530

    qr code

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,10 +1,14 @@
 "use client";
 import React,{useContext, useState} from 'react'
 import { UserContext } from '@/app/userContext';
+import {QRCodeSVG} from 'qrcode.react';
 
 
 function Register() {
     const { register, setRegister,condition,setcondition } = useContext(UserContext);
+    const [qrcode,setqrcode]=useState(false);
+    const [newidvalue,setnewidvalue]=useState(false);
+    
     
   return (
     <div className={`h-[60vh] ${condition?"phasein":"phaseout"} w-[60vw] bg-[#F7FAFE] shadow-2xl`}>
@@ -19,11 +23,20 @@ function Register() {
             <img width="32" height="32" className='hover:opacity-65 transition-all duration-100' src="https://img.icons8.com/glyph-neue/64/circled-chevron-left.png" alt="circled-chevron-left"/>
             </button>
         </div>
-        <div className='flex h-full mt-10 justify-center '>
+        <div className='flex flex-col h-auto w-full items-center gap-y-20 mt-10 justify-center '>
             <h1>Create Account</h1>
-            <div className='flex'>
-                <input type="text" />
+            <input onChange={(e)=>{
+                setnewidvalue(e.target.value)
+                setqrcode(false);
+            }} placeholder='Enter New Email ID' className='text-center outline outline-1' type="text" />
+            <div className='flex items-center justify-centerw-full'>
+                {!qrcode&&<button className={`${newidvalue ? "cursor-pointer":"hidden"}`} onClick={()=>{
+                    setqrcode(true)
+                }}>Generate QR CODE</button>}
+            {qrcode&&<QRCodeSVG value={newidvalue} />}
+
             </div>
+  
 
         </div>
     </div>
